f4ccfc0da70bc581b5e83723f1b163dd511beab3
houji <houji123>
2019-04-25 10:56:29 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/test.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class test {
+    @GetMapping("/putUserInfo")
+    public String putUserInfo() {
+
+
+
+
+
+        return "小雷你个死鬼";
+    }
+}
