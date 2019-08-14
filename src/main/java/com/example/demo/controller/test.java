9a7d3d7ee313ac4d8f293bc563cf65690dfcb86d
工友之家 <admin@example.com>
2019-08-14 18:14:22 +0800

    更新 test.java

--- src/main/java/com/example/demo/controller/test.java
+++ src/main/java/com/example/demo/controller/test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("")
 public class test {
-    @GetMapping()
+    @GetMapping("test")
     public String putUserInfo() {
 
 
